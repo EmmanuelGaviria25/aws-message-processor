b43f13af2742eefa4b37a777dc5642bef1e48e6e
Emmanuel Gaviria <emmanuel.gaviria.25@gmail.com>
2025-02-02 21:02:30 -0500

    Delete AWSMessageProcessor directory

--- AWSMessageProcessor/create_messages_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE messages (
-    id INT PRIMARY KEY,            -- Identificador único
-    name NVARCHAR(100) NOT NULL,   -- Nombre asociado al mensaje
-    value DECIMAL(10, 2) NOT NULL, -- Valor del registro
-    timestamp DATETIME NOT NULL    -- Fecha y hora del mensaje
-);
--- AWSMessageProcessor/tests/test_logger.py
+++
@@ -1,7 +0,0 @@
-from src.utils.logger import setup_logger
-import logging
-
-def test_setup_logger():
-    logger = setup_logger("TestLogger", logging.DEBUG)
-    assert logger.name == "TestLogger"
-    assert logger.level == logging.DEBUG
